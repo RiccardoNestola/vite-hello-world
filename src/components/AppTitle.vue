7b3fd9ea437658a52b20e7654de344364ecd2626
RiccardoNestola <riccardinux@gmail.com>
2022-12-23 20:03:09 +0100

    add hero and h1

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <section class="cover">
+        <div>
+            <div>
+                    <h1>La mia prima app con Vite!</h1>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+.cover{ 
+    background-image: url(https://source.unsplash.com/category/nature/);
+    background-size: cover;
+    text-align: center; 
+    padding:10px; 
+    margin:0px; 
+    min-height:600px; 
+    width:100%; 
+    display:flex; 
+    flex-direction:column; 
+    justify-content:center; 
+    color:#fff
+}
+
+
+</style>
